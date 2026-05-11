2273165f90e23b8917377c5399846721e678e1e0
BogdanK1337 <104385654+BogdanK1337@users.noreply.github.com>
2026-05-11 17:52:50 +0200

    Delete newsletter-config.js

--- newsletter-config.js
+++
@@ -1,9 +0,0 @@
-window.TECHNOVA_NEWSLETTER = {
-  enabled: true,
-  provider: "emailjs",
-  publicKey: "DhSOaqpSuDZpBrPjb",
-  serviceId: "service_d0ng7u9",
-  templateId: "template_90u0exe",
-  fromName: "TechNova Market",
-  replyTo: "kontakt@technova-market.pl"
-};
