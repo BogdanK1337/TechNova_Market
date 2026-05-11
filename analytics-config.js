43fd9b5eb1b76e7ed6b270c757078596a754b3bc
BogdanK1337 <104385654+BogdanK1337@users.noreply.github.com>
2026-05-11 18:07:13 +0200

    Add files via upload

---
+++ analytics-config.js
@@ -0,0 +1,4 @@
+﻿window.TECHNOVA_ANALYTICS = {
+  gaMeasurementId: "G-RSBYCXJ2P9",
+  clarityProjectId: "wic6o1fa55"
+};
---
+++ newsletter-config.js
@@ -0,0 +1,9 @@
+window.TECHNOVA_NEWSLETTER = {
+  enabled: true,
+  provider: "emailjs",
+  publicKey: "DhSOaqpSuDZpBrPjb",
+  serviceId: "service_d0ng7u9",
+  templateId: "template_90u0exe",
+  fromName: "TechNova Market",
+  replyTo: "kontakt@technova-market.pl"
+};
